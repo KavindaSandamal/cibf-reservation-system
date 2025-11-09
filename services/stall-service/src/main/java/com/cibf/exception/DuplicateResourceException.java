3bedd5acc48c18f11a7019be8d31ef697fe58318
TharukaFernando99 <tarakasadarenu@gmai.com>
2025-11-09 09:03:07 +0530

    updated

---
+++ services/stall-service/src/main/java/com/cibf/exception/DuplicateResourceException.java
@@ -0,0 +1,7 @@
+package com.cibf.exception;
+
+public class DuplicateResourceException extends RuntimeException {
+    public DuplicateResourceException(String message) {
+        super(message);
+    }
+}
