393d92acd1ea8f5abfb0eaa8c6bdbdeadb7dc846
Kavinda Sandamal <info.kavindasandamal@gmail.com>
2025-11-11 22:58:38 +0530

    Handle gradle issues

--- services/stall-service/src/test/java/com/cibf/stall_service/StallServiceApplicationTests.java
+++ services/stall-service/src/test/java/com/cibf/stall_service/StallServiceApplicationTests.java
@@ -3,11 +3,11 @@ package com.cibf.stall_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// Specify where to find the main application class
+@SpringBootTest(classes = com.cibf.service.StallService.class)
 class StallServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
