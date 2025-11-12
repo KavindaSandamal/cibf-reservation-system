b313440a8dd241c3b3d6be44a19e19f741320057
Hashini Fernando <139649940+hashini-fernando@users.noreply.github.com>
2025-11-13 00:22:53 +0530

    user portal updated

--- services/stall-service/src/test/java/com/cibf/stall_service/StallServiceApplicationTests.java
+++ services/stall-service/src/test/java/com/cibf/stall_service/StallServiceApplicationTests.java
@@ -1,13 +1,22 @@
-package com.cibf.stall_service;
+package com.cibf;
 
+import com.cibf.service.StallService;
+import com.cibf.repository.StallRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-// Specify where to find the main application class
-@SpringBootTest(classes = com.cibf.service.StallService.class)
+@SpringBootTest
 class StallServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private StallService stallService;
+
+   
+
+    @Test
+    void contextLoads() {
+        // Passes if Spring can create StallService with mocked repository
+    }
 }
