0a0123332c87a3e38af1203198c307af9933519d
Tharuka Sandarenu <tarakasadarenu@gmail.com>
2025-11-17 01:45:40 +0530

    Fixed handle stall availability response correctly

--- services/reservation-service/src/main/java/com/cibf/security/JwtAuthenticationFilter.java
+++ services/reservation-service/src/main/java/com/cibf/security/JwtAuthenticationFilter.java
@@ -42,7 +42,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
                 UsernamePasswordAuthenticationToken authentication =
                         new UsernamePasswordAuthenticationToken(
-                                userDetails, null, userDetails.getAuthorities());
+                                userDetails, token, userDetails.getAuthorities());
 
                 authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authentication);
