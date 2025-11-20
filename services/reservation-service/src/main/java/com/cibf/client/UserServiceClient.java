303240b0e5dca1004cfa335974dc5d1d059c1c9a
Kavinda Sandamal <info.kavindasandamal@gmail.com>
2025-11-20 17:46:05 +0530

    Update dockerfiles for caching

--- services/reservation-service/src/main/java/com/cibf/client/UserServiceClient.java
+++ services/reservation-service/src/main/java/com/cibf/client/UserServiceClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "Authentication-service", url = "http://localhost:8081/api/auth")
+@FeignClient(name = "Authentication-service", url = "http://authentication-service/api/auth")
 public interface UserServiceClient {
 
     @GetMapping("/{userId}/exists")
